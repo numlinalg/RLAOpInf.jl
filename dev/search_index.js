08f7c606691bd65a9c56052aa90ff2803ec32ba4
Documenter.jl <documenter@juliadocs.github.io>
2025-09-17 17:11:27 +0000

    build based on c97345a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#RLAOpInf","page":"Home","title":"RLAOpInf","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RLAOpInf.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
